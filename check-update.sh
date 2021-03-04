e22733ff15ebe775be0a85a3a7460f294165d7c4
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-03-04 09:17:49 -0600

    repo has moved

--- check-update.sh
+++ check-update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/ibm-cloud-docs/terraform provider-docs
+if [ -d provider-docs ]; then
+  (cd provider-docs && git pull)
+else
+  git clone https://github.com/ibm-cloud-docs/ibm-cloud-provider-for-terraform provider-docs
+fi
 
 # run the generator
 npm install
@@ -10,7 +14,7 @@ npm start
 # compare with current version in plugin
 curl -L -H "Authorization: token $GITHUB_TOKEN" https://github.com/l2fprod/vscode-terraform/raw/develop/src/data/terraform-provider-ibm.json > current-terraform-provider-ibm.json
 
-if diff terraform-provider-ibm.json current-terraform-provider-ibm.json; then
+if diff -b terraform-provider-ibm.json current-terraform-provider-ibm.json; then
   exit 0
 else
   echo "Needs an update!"
